c89efc783205d4198b1c30fb567f07be25ed8f75
何恺君 <kytonhe@macbook-8.local>
2019-03-08 17:31:04 -0500

    Initial Commit

---
+++ MarioKartGesture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarioKartGesture
+//
+//  Created by 何恺君 on 3/8/19.
+//  Copyright © 2019 Kaijun He. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
